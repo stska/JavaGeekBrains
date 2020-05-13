109b28c7cf4d3e23fdcfd3bca6d88e02819eb9b1
stska <33188480+stska@users.noreply.github.com>
2020-05-13 22:01:43 +0300

    Java 3/home_work_one

---
+++ company/Fruit.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Fruit {
+    private float fruitWeight;
+
+    public Fruit(float fruitWeight){
+        this.fruitWeight = fruitWeight;
+    }
+    public void setFruitWeight(float fruitWeight){
+        this.fruitWeight = fruitWeight;
+    }
+    public float getFruitWeight(){
+        return fruitWeight;
+    }
+
+}
